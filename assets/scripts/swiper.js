4a30cf6d5cc5dc070f3aead4946637de9ee3be00
Miguel Sena <68573307+mccavaloko@users.noreply.github.com>
2023-06-24 14:27:53 -0300

    Final Commit

---
+++ assets/scripts/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
